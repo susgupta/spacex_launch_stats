9e14e491be2a374d201166a51e777bf57618678c
susgupta <guptask.7@gmail.com>
2018-11-20 21:20:36 -0500

    Added client files for client portion

---
+++ public/precache-manifest.2f0a68baf777b7c5f2f5aedef6e0edd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1fea04739c1d451f4882",
+    "url": "/static/js/main.1fea0473.chunk.js"
+  },
+  {
+    "revision": "638c271555c9281efe32",
+    "url": "/static/js/1.638c2715.chunk.js"
+  },
+  {
+    "revision": "1fea04739c1d451f4882",
+    "url": "/static/css/main.d76ccf24.chunk.css"
+  },
+  {
+    "revision": "11ede02e5177d7360eea542873c31244",
+    "url": "/index.html"
+  }
+];
